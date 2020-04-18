0d595eaa9d6dbc724ac9aa52623a2e542840ea2c
fengzhonghe <sylwl0935@163.com>
2020-04-19 00:43:55 +0800

    Idea commit success !

--- one-idea-git/src/main/java/com/sy/HelloWorld.java
+++ one-idea-git/src/main/java/com/sy/HelloWorld.java
@@ -4,8 +4,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !");
         System.out.println("Hello World !");
-        
+
         System.out.println("Idea pull !");
         System.out.println("Idea pull 2 !");
+
+        System.out.println("Idea push !");
+
     }
 }
